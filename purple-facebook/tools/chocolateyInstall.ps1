437b931cf6848f2cde4a6c1f8b56b65effab2e6c
Michael Ray <michael.ray@rayzor.net>
2015-08-22 13:36:40 -0500

    purple-facebook: Create package v0.0.0.001400
    Create the purple-facebook Chocolatey package so that Facebook
    chat can be used in the Pidgin instant messenger.

---
+++ purple-facebook/tools/chocolateyInstall.ps1
@@ -0,0 +1,48 @@
+﻿$package_name = 'purple-facebook'
+$build_number = '14'
+$release_version = '5727ffb502c7'
+$libjson_url="https://github.com/jgeboski/purple-facebook/releases/download/downloads/libjson-glib-1.0.dll"
+$libfb_url="https://ci.voidium.net/job/purple-facebook/$build_number/artifact/source/win32-install-dir/plugins/libfacebook.dll"
+
+# Find the Pidgin install directory so that the plugin DLLs can be placed into the appropriate location
+$pidgin_subdir = 'Pidgin'
+$pidgin_plugin_subdir = 'plugins'
+$pidgin_file = 'pidgin.exe'
+$pidgin_path = ''
+
+if (Test-Path "${Env:ProgramFiles(x86)}\$pidgin_subdir\$pidgin_file")
+{
+  $pidgin_path = "${Env:ProgramFiles(x86)}\$pidgin_subdir\"
+}
+elseif (Test-Path "${Env:ProgramFiles}\$pidgin_subdir\$pidgin_file")
+{
+  $pidgin_path = "${Env:ProgramFiles}\$pidgin_subdir\"
+}
+
+if (!$pidgin_path)
+{
+  throw 'Could not find Pidgin install directory to install purple-facebook plugin'
+}
+
+# Find or create the temp directory where libjson-glib & libfacebook DLLs will be downloaded
+$app_temp = Join-Path ${Env:Temp} $package_name
+if (!(Test-Path $app_temp))
+{
+  New-Item $app_temp -Type Directory
+}
+
+# Get the DLL filename from the URL, get full temp/destination paths, then download (libjson-glib)
+$libjson_filename = $libjson_url -split '/' | Select-Object -Last 1
+$libjson_fulltemppath = Join-Path $app_temp $libjson_filename
+$libjson_fulldestpath = Join-Path $pidgin_path $libjson_filename
+Get-ChocolateyWebFile -packageName $package_name -fileFullPath $libjson_fulltemppath -url $libjson_url
+
+# Get the DLL filename from the URL, get full temp/destination paths, then download (libfacebook)
+$libfb_filename = $libfb_url -split '/' | Select-Object -Last 1
+$libfb_fulltemppath = Join-Path $app_temp $libfb_filename
+$libfb_fulldestpath = Join-Path (Join-Path $pidgin_path $pidgin_plugin_subdir) $libfb_filename
+Get-ChocolateyWebFile -packageName $package_name -fileFullPath $libfb_fulltemppath -url $libfb_url
+
+# Copy libjson-glib & libfacebook DLLs to the appropriate location
+Copy-Item -Path $libjson_fulltemppath -Destination $libjson_fulldestpath
+Copy-Item -Path $libfb_fulltemppath -Destination $libfb_fulldestpath
---
+++ purple-facebook/tools/chocolateyUninstall.ps1
@@ -0,0 +1,31 @@
+﻿$package_name = 'purple-facebook'
+
+$libjson_filename = 'libjson-glib-1.0.dll'
+$libfb_filename = 'libfacebook.dll'
+
+# Find the Pidgin install directory so that the plugin DLLs can be removed the appropriate location
+$pidgin_subdir = 'Pidgin'
+$pidgin_plugin_subdir = 'plugins'
+$pidgin_file = 'pidgin.exe'
+$pidgin_path = ''
+
+if (Test-Path "${Env:ProgramFiles(x86)}\$pidgin_subdir\$pidgin_file")
+{
+  $pidgin_path = "${Env:ProgramFiles(x86)}\$pidgin_subdir\"
+}
+elseif (Test-Path "${Env:ProgramFiles}\$pidgin_subdir\$pidgin_file")
+{
+  $pidgin_path = "${Env:ProgramFiles}\$pidgin_subdir\"
+}
+
+if (!$pidgin_path)
+{
+  throw 'Could not find Pidgin install directory to install purple-facebook plugin'
+}
+
+# Remove libjson-glib & libfacebook DLLs from the appropriate locations
+$libjson_fullpath = Join-Path $pidgin_path $libjson_filename
+Remove-Item $libjson_fullpath
+
+$libfb_fullpath = Join-Path (Join-Path $pidgin_path $pidgin_plugin_subdir) $libfb_filename
+Remove-Item $libfb_fullpath
