423078c6ecfb5276c90fe1804447c6c572ef5459
Michael Ray <michael.ray@rayzor.net>
2015-10-22 12:54:44 -0500

    purple-facebook: Update to v0.0.0.002400
    Update the purple-facebook Chocolatey package from v0.0.0.002300 to
    v0.0.0.002400.

--- purple-facebook/tools/chocolateyInstall.ps1
+++ purple-facebook/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$package_name = 'purple-facebook'
-$build_number = '23'
+$build_number = '24'
 $libjson_url="https://github.com/jgeboski/purple-facebook/releases/download/downloads/libjson-glib-1.0.dll"
 $libfb_url="https://ci.voidium.net/job/purple-facebook/$build_number/artifact/source/win32-install-dir/plugins/libfacebook.dll"
 
