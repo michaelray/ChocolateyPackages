75e84eee54eace8bf697b7afa7cc69d34d359e76
Michael Ray <git@michaelray.me>
2016-02-20 20:03:50 -0600

    dotpeek.portable: Remove version from app filename
    Remove the version from the portable app .exe file name.

--- dotpeek.portable/tools/chocolateyInstall.ps1
+++ dotpeek.portable/tools/chocolateyInstall.ps1
@@ -1,15 +1,26 @@
 ﻿$packageName = 'dotpeek.portable'
 $version = '10.0.2'
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$fileName = "dotPeek_${version}.exe"
-$destPath = Join-Path $toolsDir $fileName
 $url = "https://download.jetbrains.com/resharper/dotPeek32_${version}.exe"
 $url64 = "https://download.jetbrains.com/resharper/dotPeek64_${version}.exe"
 
-Get-ChocolateyWebFile $packageName $destPath $url $url64
+# Create temp directory if it doesn't exist
+$appTemp = [IO.Path]::Combine($ENV:Temp, $packageName)
+if (!(Test-Path $appTemp))
+{
+  New-Item -Path $appTemp -Type Directory | Out-Null
+}
+$appTempFullPath = [IO.Path]::Combine($appTemp, "dotPeek_${version}.exe")
+
+# Download package file executable to temp directory
+Get-ChocolateyWebFile $packageName $appTempFullPath $url $url64
+
+# Copy portable app exe to the appropriate location
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$destPath = Join-Path $toolsDir 'dotPeek.exe'
+Copy-Item -Path $appTempFullPath -Destination $destPath -Force
 
 # Add start menu link
 $linkPath = Join-Path ${ENV:APPDATA} "Microsoft\Windows\Start Menu\Programs\JetBrains"
 $linkName = "JetBrains dotPeek ${version}.lnk"
-New-Item -Path $linkPath -ItemType directory -Force | Out-Null
+New-Item -Path $linkPath -Type Directory -Force | Out-Null
 Install-ChocolateyShortcut -ShortcutFilePath (Join-Path $linkPath $linkName) -TargetPath $destPath
