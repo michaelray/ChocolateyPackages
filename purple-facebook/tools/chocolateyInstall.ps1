262caeb0580f3dfb89aaf75bc02823deb69e7203
Michael Ray <michael.ray@rayzor.net>
2017-06-10 14:08:10 -0500

    purple-facebook: Update to v0.9.4.128
    Update the purple-facebook Chocolatey package from v0.9.3.126 to
    v0.9.4.128.

--- purple-facebook/tools/chocolateyInstall.ps1
+++ purple-facebook/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$package_name = 'purple-facebook'
-$build_number = '126'
-$release_string = "20170330~3cbb433~c9b74a765767~$build_number"
+$build_number = '128'
+$release_string = "20170608~c40d9fa~c9b74a765767~$build_number"
 $libjson_url="https://github.com/dequis/purple-facebook/releases/download/downloads/libjson-glib-1.0.dll"
 $libjson_checksumType="sha256"
 $libjson_checksum="F6F602D99933A00BFE040330095744FEE788884318F9583149E07F85637EB699"
 $libfb_url="https://cdn.rawgit.com/michaelray/purple-facebook-bin/master/libfacebook.dll/$release_string/libfacebook.dll"
 $libfb_checksumType="sha256"
-$libfb_checksum="6FBEE2BF2F8D0810E058BA8B611A8429708DB48EEBAC4D38CC22E9FEB4189658"
+$libfb_checksum="831BDED3687F2329F6F61A6C352979F2ACF20220FF816192A7805B227EADD1ED"
 
 # Find the Pidgin install directory so that the plugin DLLs can be placed into the appropriate location
 $pidgin_subdir = 'Pidgin'
